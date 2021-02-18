f090fb11802fb75c6d65cd9b120a0f3babefee73
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-02-19 00:23:52 +0100

    toggle

--- js/script.js
+++ js/script.js
@@ -10,15 +10,29 @@ new Vue ({
 
     methods: {
 
-        mouseOver: function() {
-            this.active = true
+        // mouseOver: function() {
+        //     this.active = true
+        // },
+
+        // mouseLeave: function() {
+        //     this.active = false;
+        // },
+
+        fadeIn: function() {
+            const target = document.querySelector('.placeholder');
+            target.classList.add('visible');
         },
 
-        mouseLeave: function() {
-            this.active = false;
+        fadeOut: function() {
+            const target = document.querySelector('.placeholder');
+            target.classList.remove('visible');
+        },
+
+        scrollTop: function() {
+            document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
         }
 
-    }
+    }   
 })
 
 Vue.config.devtools = true;
