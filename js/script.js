212d7b4d05c1be9ed68fb2ec346626e46ec679d7
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-02-19 19:38:07 +0100

    v-for

--- js/script.js
+++ js/script.js
@@ -4,7 +4,59 @@ new Vue ({
 
     data: {
 
-        active: false,
+        // active: false,
+
+        blogPosts: [
+            {
+                background: 'img/h-2-blog-img-1.jpg',
+                title: 'This way, loves: a detailed guide through new design',
+                author: 'By Emily Fields',
+                day: '07',
+                year: 'MAY \'19 '
+            },
+
+            {
+                background: 'img/h-2-blog-img-2.jpg',
+                title: 'I try to give people a different way of looking at art',
+                author: 'By Emily Fields',
+                day: '07',
+                year: 'MAY \'19 '
+            },
+
+            {
+                background: 'img/h-2-blog-img-3.jpg',
+                title: 'Introduce Richard Laperrière of those amazing features',
+                author: 'By Emily Fields',
+                day: '07',
+                year: 'MAY \'19 '
+            },
+        ],
+
+        ideasDrawings: [
+            {
+                icon: '<i class="fas fa-lightbulb""></i>',
+                title:'First there is an idea',
+                paragraph:'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself'
+            },
+
+            {
+                icon: '<i class="far fa-comment" style="color: #f8d12d;"></i>',
+                title:'Then we talk about',
+                paragraph:'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself'
+            },
+
+            {
+                icon: '<i class="fas fa-cloud" style="color: #80465f;"></i>',
+                title:'And we think about',
+                paragraph:'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself'
+            },
+            
+            {
+                icon: '<i class="fas fa-pencil-alt" style="color: #eb6950;"></i>',
+                title:'So we draw along',
+                paragraph:'Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself'
+            },
+        ]
 
     },
 
